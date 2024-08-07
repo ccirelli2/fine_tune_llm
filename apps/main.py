23e18e784318de8794fcf839642cc4cb7370398e
ccirelli2 <ccirelli2@gmail.com>
2024-08-07 07:48:13 -0400

    -q

--- apps/main.py
+++ apps/main.py
@@ -3,4 +3,42 @@ import pandas as pd
 
 st.title("GSU Institute for Insight - LLM Fine Tuning Trial Applicaiton")
 
+st.markdown("----")
+st.write("")
+st.write("")
+
+st.subheader("Introduction")
+st.write("""
+    This application is designed to facilitate comparing the performance of
+    large language models on their ability to extract various financial metrics
+    from SEC Edgar filings."""
+)
+st.write("""
+    The purpose of this page is to providen an explanation and basic instructions
+    for how to use the application, generate experiments, trials and run
+    validations."""
+)
+
+st.write("")
+st.write("")
+
+# Section I: Create Experiment
+st.subheader("Section I: Create Experiment")
+st.write("")
+st.write("")
+
+# Section II: Create Trial & Trial Parameters
+st.subheader("Section II: Create Trial & Trial Parameters")
+st.write("")
+st.write("")
+
+# Section III: Inspection Extractions
+st.subheader("Section III: Inspect & Inspect Extractions")
+st.write("")
+st.write("")
+
+# Section IV: Validation Results & Model Comparison
+st.subheader("Section IV: Validation Results & Model Comparison")
+st.write("")
+st.write("")
 
